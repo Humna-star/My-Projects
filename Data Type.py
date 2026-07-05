8db3884f6a2bc6af9cae32ac92dff3d0c7ae9492
Humna-star <arsalank848@gmail.com>
2026-07-05 20:35:25 +0500

    Create Data Type.py

---
+++ Data Type.py
@@ -0,0 +1,12 @@
+#Let's Check the Datatype of different values
+a= 10
+print("Type of a:", type(a)) #int
+
+b= 10.5
+print("Type of b:", type(b)) #float
+
+c= "coding"
+print("Type of c:", type(c)) #str
+
+d= True
+print("Type of d:", type(d)) #bool
