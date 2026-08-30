e55b92c50eb359c1e44fdbdc3629870584001bc3
Humna Arsalan Khan <arsalank848@gmail.com>
2026-08-30 21:14:04 +0500

    Add files via upload

---
+++ Mathematical Operations.py
@@ -0,0 +1,10 @@
+import math
+print('The floor and ceiling value 169.796 are: ' + str(math.ceil(169.796)) + ',' + str(math.floor(169.796)))
+
+x = 45578989987
+y = -68786545
+print('The value of x after copying the sign from y is:' + str(math.copysign(x,y)))
+
+print('Absolute value of -7894 and 6856 are:' + str(math.fabs(-7894)) + ', ' + str(math.fabs(6856)))
+
+print('The GCD of 8765 and 6787:' + str(math.gcd(8765,6787)))
