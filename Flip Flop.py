43ac1ddf830dd3c4f2dc05bb811eb78af56204c3
Humna Arsalan Khan <arsalank848@gmail.com>
2026-09-12 20:33:11 +0500

    Add files via upload

---
+++ Flip Flop.py
@@ -0,0 +1,17 @@
+# Function to check palindroom or not
+def palindrome(r):
+    e = len(r) - 1
+    s = 0
+    while (s < e):
+        if (r[s] != r[e]):
+            return False
+        s += 1
+        e -= 1
+    return True
+
+r = (1,2,3,3,2,1)
+
+if (palindrome(r)):
+    print("The tuple is Flip Flop")
+else:
+    print("The tuple is not Flip Flop")
