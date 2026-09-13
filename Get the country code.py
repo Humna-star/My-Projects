8c4dd43ea6c0c9448f7cef85d0f43da413b24664
Humna Arsalan Khan <arsalank848@gmail.com>
2026-09-13 20:57:26 +0500

    Add files via upload

---
+++ Get the country code.py
@@ -0,0 +1,101 @@
+country_code = {
+    "United States": "+1",
+    "Canada": "+1",
+    "United Kingdom": "+44",
+    "Australia": "+61",
+    "Germany": "+49",
+    "France": "+33",
+    "Japan": "+81",
+    "China": "+86",
+    "India": "+91",
+    "Brazil": "+55",
+    "Pakistan": "+92",
+    "Kenya": "+254",
+    "Mexico": "+52",
+    "Vietnam": "+84",
+    "Qatar": "+974",
+    "South Africa": "+27",
+    "Singapore": "+65",
+    "Sweden": "+46",
+    "Switzerland": "+41",
+    "South Korea": "+82",
+    "Denmark": "+45",
+    "Poland": "+48",
+    "Zimbabwe": "+263",
+    "Luxembourg": "+352",
+}
+
+print("\nCountry codes for United State-")
+print(country_code.get('United State', 'Not Found'))
+
+print("\nCountry codes for Nepal-")
+print(country_code.get('Nepal', 'Not Found'))
+
+print("\nCountry codes for Canada-")
+print(country_code.get('Canada', 'Not Found'))
+
+print("\nCountry codes for Germany-")
+print(country_code.get('Germany', 'Not Found'))
+
+print("\nCountry codes for India-")
+print(country_code.get('India', 'Not Found'))
+
+print("\nCountry codes for Brazil-")
+print(country_code.get('Brazil', 'Not Found'))
+
+print("\nCountry codes for Pakistan-")
+print(country_code.get('Pakistan', 'Not Found'))
+
+print("\nCountry codes for Kenya-")
+print(country_code.get('Kenya', 'Not Found'))
+
+print("\nCountry codes for Mexico-")
+print(country_code.get('Mexico', 'Not Found'))
+
+print("\nCountry codes for Vietnam-")
+print(country_code.get('Vietnam', 'Not Found'))
+
+print("\nCountry codes for Qatar-")
+print(country_code.get('Qatar', 'Not Found'))
+
+print("\nCountry codes for South Africa-")
+print(country_code.get('South Africa', 'Not Found'))
+
+print("\nCountry codes for Singapore-")
+print(country_code.get('Singapore', 'Not Found'))
+
+print("\nCountry codes for Sweden-")
+print(country_code.get('Sweden', 'Not Found'))
+
+print("\nCountry codes for Switzerland-")
+print(country_code.get('Switzerland', 'Not Found'))
+
+print("\nCountry codes for South Korea-")
+print(country_code.get('South Korea', 'Not Found'))
+
+print("\nCountry codes for Denmark-")
+print(country_code.get('Denmark', 'Not Found'))
+
+print("\nCountry codes for Poland-")
+print(country_code.get('Poland', 'Not Found'))
+
+print("\nCountry codes for Zimbabwe-")
+print(country_code.get('Zimbabwe', 'Not Found'))
+
+print("\nCountry codes for Luxembourg-")
+print(country_code.get('Luxembourg', 'Not Found'))
+
+print("\nCountry codes for Japan-")
+print(country_code.get('Japan', 'Not Found'))
+
+print("\nCountry codes for China-")
+print(country_code.get('China', 'Not Found'))
+
+print("\nCountry codes for France-")
+print(country_code.get('France', 'Not Found'))
+
+print("\nCountry codes for Australia-")
+print(country_code.get('Australia', 'Not Found'))
+
+print("\nCountry codes for United Kingdom-")
+print(country_code.get('United Kingdom', 'Not Found'))
